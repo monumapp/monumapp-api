d0fc6ca97ebbc473fca7997f29f2b32ba2f66244
Vitor Henrique <vhsouzaeng@gmail.com>
2020-09-09 14:05:40 -0300

    initial commit

---
+++ src/test/java/com/monumapp/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monumapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
